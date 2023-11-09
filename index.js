19879157b74be0b9035ba5b0bb423784827a0fa9
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-10 03:47:32 +0800

    update

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const UserReportModel = require('./models/user_report_model');
 const AdminRegistrationModel = require('./models/admin_registration_model');
 const db = require('./config/db')
 
-const port = 9000;
+const port = process.env.PORT || 80;
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
 
 
-app.listen(process.env.PORT || port,()=>{
+app.listen(port,()=>{
     console.log(`Server Listening on Port http://localhost:${port}`);
 })

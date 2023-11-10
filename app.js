40cdac90ff672324e6c65090f16da4f2cae46299
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-11 03:02:10 +0800

    commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const app = express();
+app.use(cors());
+
+
 const UserRoute = require("./routers/user_information_router");
 const UserReportRoute = require("./routers/user_report_router");
 const AdminRegistrationRoute = require("./routers/admin_registration_router");
@@ -14,8 +18,6 @@ const AdminResponseToBarangayRoute = require("./routers/admin_response_to_barang
 const InquiryRoute = require("./routers/inquiry_routes");
 const AdminNotificationRoute = require("./routers/admin_notifications_router");
 
-const app = express();
-app.use(cors());
 
 // Set the body-parser middleware with the increased limit
 app.use(bodyParser.json({ limit: '50mb' }));

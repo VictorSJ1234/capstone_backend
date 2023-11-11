ffc4b4ccf79619ab57ec63f98075d6d7dec4d256
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-11 22:26:09 +0800

    update

---
+++ allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    process.env.APP_CLIENT,
+    process.env.APP_DOMAIN,
+    'http://localhost:3000',
+    'http://localhost:5000',
+    'http://localhost:80',
+    'https://www.mosquinator.online',
+    'http://www.mosquinator.online'
+]

0955c7b95dde2012216be1e072a0ade71710682a
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-11 18:53:10 +0800

    update

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ const app = require("./app");
 const UserModel = require('./models/user_information_model');
 const UserReportModel = require('./models/user_report_model');
 const AdminRegistrationModel = require('./models/admin_registration_model');
-const db = require('./config/db');
-const axios = require("axios");
+const db = require('./config/db')
 
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    axios.get("https://mosquinator-backend-20075696f4d1.herokuapp.com")
-    .then(function (response) {
-        res.header("Access-Control-Allow-Origin", "https://www.mosquinator.online"); 
-        res.send("Hello");
-      })
-      .catch(function (error) {
-        console.log(error);
-    });
+    res.send("Hello World")
 })
 
 

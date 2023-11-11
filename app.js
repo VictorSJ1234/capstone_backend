f447caee9ca5dbcc359ef1f4de34f7147a449ddd
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-11 18:06:33 +0800

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const InquiryRoute = require("./routers/inquiry_routes");
 const AdminNotificationRoute = require("./routers/admin_notifications_router");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://mosquinator-backend-20075696f4d1.herokuapp.com", credentials: true }))
 
 // Set the body-parser middleware with the increased limit
 app.use(bodyParser.json({ limit: '50mb' }));

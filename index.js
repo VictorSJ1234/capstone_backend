fa0c1b1367ef1cef7e4c46c4f712b71400432d4c
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-12 15:36:35 +0800

    update

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const UserReportModel = require('./models/user_report_model');
 const AdminRegistrationModel = require('./models/admin_registration_model');
 const db = require('./config/db');
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    next();
-  });
-  
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{

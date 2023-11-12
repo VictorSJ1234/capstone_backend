25084604ec78247c6a50c944530a63c52fa30b0f
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-12 16:32:24 +0800

    update

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ const UserModel = require('./models/user_information_model');
 const UserReportModel = require('./models/user_report_model');
 const AdminRegistrationModel = require('./models/admin_registration_model');
 const db = require('./config/db');
-const cors = require('cors');
 
-const corsOptions = {
-    origin: 'https://www.mosquinator.online', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-    optionsSuccessStatus: 204, 
-};
-
-app.use(cors(corsOptions));
-  
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{

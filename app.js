8ac8d34a9311d40f692df440a1cc255d32b8c951
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-12 16:40:23 +0800

    update

--- app.js
+++ app.js
@@ -15,7 +15,14 @@ const InquiryRoute = require("./routers/inquiry_routes");
 const AdminNotificationRoute = require("./routers/admin_notifications_router");
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: 'https://www.mosquinator.online', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    optionsSuccessStatus: 204, 
+};
+
+app.use(cors(corsOptions));
 
 // Set the body-parser middleware with the increased limit
 app.use(bodyParser.json({ limit: '50mb' }));

fa636b2f0949d173e1f2578109c6900e6ce01833
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-12 15:01:01 +0800

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const AdminNotificationRoute = require("./routers/admin_notifications_router");
 
 const app = express();
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://mosquinator-backend-20075696f4d1.herokuapp.com"); 
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });

144cd821b8688bc8c9d1f8747dd2913ba2cc0300
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2023-11-14 03:13:30 +0800

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const UserReportModel = require('./models/user_report_model');
 const AdminRegistrationModel = require('./models/admin_registration_model');
 const db = require('./config/db');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/',(req,res)=>{
     res.send("Hello World")

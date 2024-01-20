0ba93d44774fa8b703f9e88b986c3d55f9f1e133
RjSanJuan <84064817+RjSanJuan@users.noreply.github.com>
2024-01-20 10:33:47 +0800

    with robots.txt

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const app = require("./app");
 const UserModel = require('./models/user_information_model');
 const UserReportModel = require('./models/user_report_model');
@@ -5,6 +7,12 @@ const AdminRegistrationModel = require('./models/admin_registration_model');
 const db = require('./config/db');
 
 const port = process.env.PORT || 5000;
+
+app.get('/robots.txt', (req, res) => {
+    const filePath = path.join(__dirname, 'robots.txt');
+    const stream = fs.createReadStream(filePath);
+    stream.pipe(res);
+  });
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
